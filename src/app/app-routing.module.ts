0b2b7622ebbd5edab7d6eacac154f729f05f3d8e
Powarox219 <moriniere.robin1877@gmail.com>
2022-02-03 21:16:29 +0100

    Add route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
+import { Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { TestComponent } from './test/test.component';
 
-
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'test', component: TestComponent },
+]
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+    declarations: [],
+    imports: [
+        CommonModule
+    ]
 })
 export class AppRoutingModule { }
